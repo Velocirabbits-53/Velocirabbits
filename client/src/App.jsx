552fc26574f2d8583510013c68f80e54b6a6b076
Emma Vargas <evargas8112@gmail.com>
2025-03-01 15:33:05 -0800

    added login page to routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import CreatePoll from './pages/CreatePoll';
 import VoteNow from './pages/VotingPage';
+import LoginPage from './pages/Login-Page';
+
 // import PastPolls from './pages/PastPolls';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path='/' element={<Dashboard />} />
         <Route path='/create-poll' element={<CreatePoll />} />
         <Route path='/vote-now' element={<VoteNow />} />
+        <Route path='/login' element={<LoginPage />} />
+
         {/* <Route path='/past-polls' element={<PastPolls />} /> */}
       </Routes>
     </BrowserRouter>
