54598df7d81dd79f6a7e1938846ab3db3733e2e4
MoniqueNunez <mnunezchiro@gmail.com>
2025-03-01 14:34:48 -0600

    add

--- server/src/routers/routers.js
+++ server/src/routers/routers.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticationController = require('../controllers/authenticationController')
+const pollController = require ('../controllers/pollController')
 
 //post request to handle logins
 router.post('/login', authenticationController.login,(req, res) => {
@@ -12,6 +13,12 @@ router.post('/login', authenticationController.login,(req, res) => {
 router.post('/register', authenticationController.register,(req, res) => {
     return res.status(200).send('This is the working register submit button');
   });
+
+router.post('/createPoll', pollController.createPoll, (req, res) => {
+  return res.status(200).send('This is the create poll page');
+});
+
+
 
 //Export the router---------------------------
 module.exports = router;
