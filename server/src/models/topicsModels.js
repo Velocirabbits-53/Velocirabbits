51279e47ffd3ae15a8fd62f3516b648421e2dd98
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-03-01 10:46:46 -0500

    created models file for db

--- server/src/models/topicsModels.js
+++ server/src/models/topicsModels.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+// const MONGO_URI =
+
+mongoose
+  .connect(MONGO_URI, {
+    // options for the connect method to parse the URI
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // sets the name of the DB that our collections are part of
+    dbName: 'polls',
+  })
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch((err) => console.log(err));
+
+  const Schema = mongoose.Schema;
