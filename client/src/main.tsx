7b8dd4db890a091fc01948b16b74247a334c6526
Ataraxist <CAntico@gmail.com>
2025-03-05 12:24:21 -0800

    Refactor: Typescripting enforced on main/createPoll/Register
    Imported types from types.ts file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,21 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+const root = createRoot(rootElement as HTMLElement);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
